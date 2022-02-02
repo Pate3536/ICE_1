f50b9d28d2f6ad04a03b27da38b3ce6338afe4cf
pate3536 <pate3536@sheridancollege.ca>
2022-02-02 11:12:25 -0500

    this is First commen tof prince patel

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * A class to model Card Objects.
  * Created in Week 2 for the cloning repository demo
  * @author dancye, 2019
+ * my name is prince.
  */
 public class Card 
 {
